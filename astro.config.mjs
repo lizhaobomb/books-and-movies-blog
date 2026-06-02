347fbcb5d0e0d92bc87966bf726407dff6bc20f9
lizhao <lizhao@ihuyue.cn>
2026-06-02 18:30:30 +0800

    修复开发环境路径问题

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   site: 'https://lizhaobomb.github.io',
-  base: '/books-and-movies-blog',
+  base: isProduction ? '/books-and-movies-blog' : '/',
   output: 'static',
   integrations: [
     tailwind(),
