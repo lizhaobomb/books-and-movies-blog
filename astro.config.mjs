084fc7663a6388c8418008e6691a66fa60e8213a
lizhao <lizhao@ihuyue.cn>
2026-06-03 10:01:31 +0800

    配置自定义域名 www.dreamshelf.cc.cd

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://lizhaobomb.github.io',
-  base: '/books-and-movies-blog',
+  site: 'https://www.dreamshelf.cc.cd',
+  base: '/',
   output: 'static',
   integrations: [
     tailwind(),
